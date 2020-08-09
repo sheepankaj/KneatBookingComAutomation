aa5c9ef996b0adf678d746211b4e4f330beb334c
Pankaj Kumar Shee <sheepankaj@gmail.com>
2020-08-09 22:42:39 +0100

    Final Commit

---
+++ KneatBookingComTest/Framework/SetUp/TestBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace KneatBookingComTest.Framework.SetUp
+{
+    public static class TestBase
+    {
+        #region Base Url
+        private static string UrlBookingCom = "https://www.booking.com/";
+        public static string BaseUrl = UrlBookingCom;
+
+        #endregion
+
+        #region Browser
+        public static string BaseBrowser = "Chrome";
+
+        #endregion
+    }
+}
